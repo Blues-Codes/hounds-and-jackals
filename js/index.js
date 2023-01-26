880948022a9050e0cc977a8e626437c33b412474
Jeanette Williamson <JeanetteWilliamson10@yahoo.com>
2023-01-26 17:32:51 -0500

    conditionals 6-20 8-10 not working yet

--- js/index.js
+++ js/index.js
@@ -10,9 +10,11 @@ const coinImage = document.querySelector(".coinImg");
 const coinMove1 = document.querySelector(".coinMove1");
 const coinMove2 = document.querySelector(".coinMove2");
 const coinMove3 = document.querySelector(".coinMove3");
-const goldCoinHead = "../Images/coin-head-removebg-preview.png";
-const goldCoinTail = "../Images/coin-tail-removebg-preview.png";
-
+const goldCoinHead = "../images/coin-head.png";
+const goldCoinTail = "../images/coin-tail.png";
+const log = document.querySelector(`.log`);
+const gameScreen = document.querySelector('#gamecontainer')
+console.log(gameScreen)
 //const logo = new Image() // logo
 
 // how many moves in the current round
@@ -136,6 +138,11 @@ function flipMoves() {
   }
 }
 function checkForMoves() {
+  if (playing.currentPosition >= 29 && playing.name === "hounds" || playing.currentPosition >= 29 && playing.name === "jackals") {
+    gameScreen.style.display = "none"
+    
+    return
+  }
   round = 0;
   const heads = coinMoves.filter((current) => current === "heads");
   const tails = coinMoves.filter((current) => current === "tails");
@@ -153,9 +160,9 @@ function checkForMoves() {
     log.innerHTML = `<h3> Heads! ${playing.name} got ${currentMove} moves.</h3>`;
   }
   if (tails.length === 3) {
+    currentMove = moves[3].moves;
     log.innerHTML = `<h3>WOW, 3 Tails! ${playing.name} got ${currentMove} moves.</h3>`;
 
-    currentMove = moves[3].moves;
   }
   console.log("💃", currentMove);
   console.log("💻", playing);
@@ -165,12 +172,13 @@ function checkForMoves() {
    flipCoinMove.disabled = true;
   setTimeout(() => {
     flipCoinMove.disabled = false;
-    coinMove1.src = "";
-    coinMove2.src = "";
-    coinMove3.src = "";
-  }, 1500);
-  if (playing.name === "hounds") {
+    coinMove1.src = "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=";
+    coinMove2.src = "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=";
+    coinMove3.src = "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=";
+  }, 0);
+  if (playing.name === "hounds" && playing.currentPosition < 29) {
     let newHPos = playing.currentPosition + currentMove;
+    console.log("testing")
     ctx.clearRect(playing.x, playing.y, playing.width, playing.height);
 
     ctx.drawImage(
@@ -181,40 +189,6 @@ function checkForMoves() {
       80
     );
     playing.currentPosition += currentMove;
-    console.log(playing.currentPosition);
-
-    hounds.y = whiteCords[newHPos].y;
-    hounds.x = whiteCords[newHPos].x;
-    playing = jackals;
-    console.log(playing);
-    return;
-  }
-  if (playing.name === "jackals") {
-    let newJPos = (playing.currentPosition += currentMove);
-    ctx.clearRect(playing.x, playing.y, playing.width, playing.height);
-    ctx.drawImage(
-      playing.image,
-      blackCords[newJPos].x,
-      blackCords[newJPos].y,
-      playing.width,
-      playing.height
-    );
-  playing.currentPosition += currentMove;
-  console.log(playing.currentPosition);
-  jackals.x = blackCords[newJPos].x;
-    jackals.y = blackCords[newJPos].y;
-    console.log(playing.currentPosition);
-
-    if (playing.currentPosition === 6 && playing.name === "jackals") {
-      // update();
-      ctx.drawImage(
-        playing.image,
-        blackCords[20].x,
-        blackCords[20].y,
-        playing.width,
-        playing.height
-      );
-    }
     if (playing.currentPosition === 6 && playing.name === "hounds") {
       // update();
       ctx.drawImage(
@@ -225,10 +199,105 @@ function checkForMoves() {
         playing.height
       );
     }
-    playing = hounds;
+    if (playing.currentPosition === 20 && playing.name === "hounds") {
+      // update();
+      ctx.drawImage(
+        playing.image,
+        whiteCords[6].x,
+        whiteCords[6].y,
+        playing.width,
+        playing.height
+      );
+    }
+    if (playing.currentPosition === 8 && playing.name === "hounds") {
+      // update();
+      ctx.drawImage(
+        playing.image,
+        whiteCords[10].x,
+        whiteCords[10].y,
+        playing.width,
+        playing.height
+      );
+    }
+
+    if (playing.currentPosition === 10 && playing.name === "hounds") {
+      // update();
+      ctx.drawImage(
+        playing.image,
+        whiteCords[8].x,
+        whiteCords[8].y,
+        playing.width,
+        playing.height
+      );
+    }
+    console.log(playing.currentPosition);
+
+    hounds.y = whiteCords[newHPos].y;
+    hounds.x = whiteCords[newHPos].x;
+    playing = jackals;
     console.log(playing);
     return;
   }
+  if (playing.name === "jackals" && playing.currentPosition < 29) {
+    let newJPos = (playing.currentPosition + currentMove);
+    ctx.clearRect(playing.x, playing.y, playing.width, playing.height);
+    ctx.drawImage(
+      playing.image,
+      blackCords[newJPos].x,
+      blackCords[newJPos].y,
+      playing.width,
+      playing.height
+    );
+  playing.currentPosition += currentMove;
+  if (playing.currentPosition === 6 && playing.name === "jackals") {
+    // update();
+    ctx.drawImage(
+      playing.image,
+      blackCords[20].x,
+      blackCords[20].y,
+      playing.width,
+      playing.height
+    );
+  }
+  if (playing.currentPosition === 20 && playing.name === "jackals") {
+    // update();
+    ctx.drawImage(
+      playing.image,
+      blackCords[6].x,
+      blackCords[6].y,
+      playing.width,
+      playing.height
+    );
+  }
+  if (playing.currentPosition === 8 && playing.name === "jackals") {
+    // update();
+    ctx.drawImage(
+      playing.image,
+      blackCords[10].x,
+      blackCords[10].y,
+      playing.width,
+      playing.height
+    );
+  }
+
+  if (playing.currentPosition === 10 && playing.name === "jackals") {
+    // update();
+    ctx.drawImage(
+      playing.image,
+      blackCords[8].x,
+      blackCords[8].y,
+      playing.width,
+      playing.height
+    );
+  }
+    console.log(playing.currentPosition);
+    jackals.x = blackCords[newJPos].x;
+    jackals.y = blackCords[newJPos].y;
+    console.log(playing.currentPosition);
+    playing = hounds;
+    console.log(playing);
+      return;
+    }
 }
 function startGame() {
   document.getElementById("startplaywin").style.display = "flex";
@@ -241,6 +310,30 @@ function startGame() {
   canvas.style.visibility = "visible";
   canvas.style.display = "inherit";
 }
+
+// function gameOver() {
+
+//   gameOn = false
+
+//   console.log("Game over")
+//   clearInterval(animationId)
+//   clearInterval(intervalId)
+//   obstaclesArray = []
+
+//   ctx.clearRect(0,0,500,700)
+//   ctx.fillStyle = 'black'
+//   ctx.fillRect(0,0,500,700)
+
+//   if (score > 14) {
+//     ctx.fillStyle = "white"
+//     ctx.font = '40px serif'
+//     ctx.fillText("You've won!", 150, 200)
+//   } else {
+//     ctx.fillStyle = "white"
+//     ctx.font = '40px serif'
+//     ctx.fillText("You lose!", 150, 200)
+//   }
+// }
 
 function animate() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
