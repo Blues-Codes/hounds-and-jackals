8a7337e95b0279bd85a17ec9e2290b6ea1e76da0
Jeanette Williamson <JeanetteWilliamson10@yahoo.com>
2023-01-27 12:56:45 -0500

    updated image tags

--- js/index.js
+++ js/index.js
@@ -84,11 +84,11 @@ const hounds = {
 function flipCoin() {
   const rng = Math.floor(Math.random() * 2);
   if (rng === 0) {
-    coinImage.src = "../Images/coin-head-silver.png";
+    coinImage.src = "images/coin-head-silver.png";
     playing = jackals;
     log1.innerHTML = `<h3>Heads! ANUBIS be praised! Jackals goes first!</h3>`;
   } else {
-    coinImage.src = "../Images/coin-tail-silver.png";
+    coinImage.src = "images/coin-tail-silver.png";
     playing = hounds;
     log1.innerHTML = `<h3>Tails! May RA bless you! Hounds goes first!</h3>`;
   }
